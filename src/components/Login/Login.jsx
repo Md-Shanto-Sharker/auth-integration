3aeac2048e42270539f2d7d3c2ee7bd03fe950dc
SHANTO <rabbymd55555@gmail.com>
2025-07-01 10:55:16 +0600

    added context

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { use } from "react";
 import { Link } from "react-router";
+import { AuthContext } from "../../context/AuthContext";
 
 const Login = () => {
+  const { signInUser } = use(AuthContext);
+  console.log(signInUser);
+
   const handleLogin = (e) => {
     e.preventDefault();
-    console.log("hello");
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log("hello", email, password);
+
+    signInUser(email, password)
+      .then((userCredential) => {
+        console.log(userCredential);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
   return (
     <div className="card mt-20 mx-auto bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
@@ -12,10 +26,20 @@ const Login = () => {
       <div className="card-body">
         <form onSubmit={handleLogin} className="fieldset">
           <label className="label">Email</label>
-          <input type="email" className="input" placeholder="Email" />
+          <input
+            type="email"
+            name="email"
+            className="input"
+            placeholder="Email"
+          />
 
           <label className="label">Password</label>
-          <input type="password" className="input" placeholder="Password" />
+          <input
+            type="password"
+            name="password"
+            className="input"
+            placeholder="Password"
+          />
           <div>
             <a className="link link-hover">Forgot password?</a>
           </div>
--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,15 +1,31 @@
 import React from "react";
 import { AuthContext } from "./AuthContext";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "./../Firebase/Firebase.init";
 const AuthProvider = ({ children }) => {
   const createUser = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
+  const signInUser = (email,password)=>{
+    return signInWithEmailAndPassword(auth, email, password)
+  }
+
   const userInfo = {
-    createUser
+    createUser,
+    signInUser
   };
+
+
+  onAuthStateChanged(auth, (user) => {
+  if (user) {
+    console.log('has current user',user);
+  } else {
+    console.log('current',user);
+  }
+});
+
+
   return <AuthContext value={userInfo}>{children}</AuthContext>;
 };
 
